e78de9dcdc15966ce6aebdb2d7377148fb175c9c
remytembodev <remytembo4@gmail.com>
2023-08-27 01:04:58 +0200

    adding responsive

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
 <?php
- echo "hello world";
- header("location:showbooks.php");
+
+ 
 ?>
--- src/showbooks.php
+++ src/showbooks.php
@@ -1,4 +1,6 @@
+<?php
 
+?>
 
 
 
@@ -8,35 +10,149 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Affichage des livres</title>
+    <link rel="stylesheet" href="../style/header/home.css">
+    <link rel="stylesheet" href="../style/showbooks/show.css">
 </head>
 <body>
-    <div class="container" id="affichage-livre">
+     <!-- NAVBAR--> 
+     <header>
+        <div class="logo"><img src="../img/isipa.png" alt=""></div>
+        <input type="checkbox" id="nav_check" hidden>
+        <nav>
+            <ul>
+                <li><a href="#" >Home</a></li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Se connecter</a></li>
+            </ul>
+        </nav>
+        <div id="cont">
+            <img src="../img/Clip path group.png" alt="" style="padding-right: 15px;">
+            <img src="../img/skill-icons_twitter 1.png" alt=""  style="padding-right: 15px;">
+            <img src="../img/logos_facebook 1.png" alt=""  style="padding-right: 15px;">
+            <img src="../img/skill-icons_instagram 1.png" alt=""  style="padding-right: 15px;">
+        </div>
+        <label for="nav_check" class="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </label>
+
+    </header>
+    <!--FIN NAVBAR--> 
+    <div class="containerbooks" id="affichage-livre">
         <div class="showbooks">
             <div class="show">
                 <img src="../img/7 1.svg" alt="">
-            </div>
-            <div class="show">
-                <img src="" alt="">
-            </div>
-            <div class="show">
-                <img src="../img/1 1.svg" alt="">
-            </div>
-            <div class="show">
-                <img src="../img/2 2.svg" alt="">
-            </div>
-            <div class="show">
-                <img src="../img/3 1.svg" alt="">
-            </div>
-            <div class="show">
-                <img src="../img/3 2.svg" alt="">
-            </div>
-            <div class="show">
-                <img src="../img/4 1.svg" alt="">
+                <button type="button">Lire</button>
             </div>
             <div class="show">
                 <img src="../img/4 2.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/7 1.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/4 2.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/7 1.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/4 2.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/1 1.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/2 2.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/3 1.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/3 2.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/4 1.svg" alt="">
+                <button type="button">Lire</button>
+            </div>
+            <div class="show">
+                <img src="../img/4 2.svg" alt="">
+                <button type="button">Lire</button>
             </div>
         </div>
     </div>
+
+    <footer>
+        <p style=" font-family: inter; font-size: 20px; margin-bottom: 20px;">Type des livres</p>
+        <div class="footer-livres">
+            <div>
+                <ul>
+                    
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-foot">
+            <nav class="navf">
+              <ul>
+                <li><a href="#" >Home</a></li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Se connecter</a></li>
+              </ul>
+            </nav>
+            <div class="logo-foot">
+                <img src="img/Clip path group.png" alt="" style="padding-right: 15px;">
+                <img src="img/skill-icons_twitter 1.png" alt=""  style="padding-right: 15px;">
+                <img src="img/logos_facebook 1.png" alt=""  style="padding-right: 15px;">
+                <img src="img/skill-icons_instagram 1.png" alt=""  style="padding-right: 15px;">
+            </div>
+        </div>
+        <p style="color: white; text-align: center; padding-top: 30px;"> copyright in 2023 by Scorpion Lab </p>
+    </footer>
 </body>
 </html>
